d6504e2b6f60a26cb97b743c8723bce8e5e49838
rdtmaster <hakeemalbahr@gmail.com>
2023-05-11 00:06:54 +0300

    interface reworking - css changes (no compat with reddit's compact.css, add `.hidden` class and change `display` properties for some classes)

--- static/js.js
+++ static/js.js
@@ -1,19 +1,20 @@
 const baseURL=''; //same domain
 function unimpl(){
 	alert('Unimplemented!!!');
+	return false;
 }
 function hide(elem){
-	elem.style.display = 'none';
+	elem.classList.add('hidden');
 }
 
 function show(elem){
-	elem.style.display = 'block';
+	elem.classList.remove('hidden');
 }
 function togDisplay(elem){
-	if (elem.style.display === 'none') {
-		elem.style.display = 'block';
+	if (elem.classList.contains('hidden')) {
+		show(elem);
 	} else {
-		elem.style.display = 'none';
+		hide(elem);
 	}
 }
 
@@ -34,47 +35,65 @@ function optionsDisplay(thingId){
 	togDisplay(subm.getElementsByClassName('options_expando')[0]);
 			
 }
+function isHidden(elem){
+	return elem.classList.contains('hidden');
+}
 
+function isShown(elem){
+	return !isHidden(elem);
+}
 function editDisplay(that){
 	const thing = that.parentNode.parentNode;
 	const expando = thing.getElementsByClassName('expando')[0];
-	if (expando.style.display == 'none'){
+	
+	if (isHidden(expando)){
 		show(expando);
 	}
 	togDisplay(expando.getElementsByClassName('usertext-edit')[0]);
-	
-	
-	/*
-	
-	
-	togDisplay(userTextBody.getElementsByClassName('md')[1]);
-	togDisplay(userTextBody);
-	
-	
-	togDisplay(userTextBody.getElementsByClassName('md')[0]);
-	const btn = subm.getElementsByClassName('selftext')[0];
-	if(btn.classList.contains('expanded')){
-		btn.classList.remove('expanded');
-		btn.classList.add('collapsed');
-	} else {
-		btn.classList.remove('collapsed');
-		btn.classList.add('expanded');
-	}
-	*/
+	togDisplay(expando.getElementsByClassName('usertext-body')[0]);
+	return false;
 	
 }
-function userTextDisplay(thingId){
-	const subm = document.getElementsByClassName('id-'+thingId)[0];
-	const userTextBody = subm.getElementsByClassName('expando')[0];
-	const btn = subm.getElementsByClassName('selftext')[0];
-	togDisplay(userTextBody);
-	if(btn.classList.contains('expanded')){
-		btn.classList.remove('expanded');
-		btn.classList.add('collapsed');
+function editFormDisplay(that){
+	const uf = that.parentNode.parentNode.getElementsByClassName('usertext')[0];
+	togDisplay(uf.getElementsByClassName('md')[0]);
+	togDisplay(uf.getElementsByClassName('usertext-edit')[0]);
+	return false;
+}
+function replyFormDisplay(that){
+	const thing = that.parentNode.parentNode.parentNode;
+	togDisplay(thing.getElementsByClassName('commentreply')[0]);
+	return false;
+}
+
+
+
+function postBodyDisplay(that){
+	const thing = that.parentNode.parentNode;
+	const expando = thing.getElementsByClassName('expando')[0];
+	if (expando){
+		
+		const utEdit = expando.getElementsByClassName('usertext-edit')[0];
+		if (utEdit){
+			hide(utEdit);
+		}
+		const utBody = expando.getElementsByClassName('usertext-body')[0];
+		if (utBody){
+			show(utBody);
+		}
+		togDisplay(expando);
+		
+	}	
+	
+	if(that.classList.contains('expanded')){
+		that.classList.remove('expanded');
+		that.classList.add('collapsed');
 	} else {
-		btn.classList.remove('collapsed');
-		btn.classList.add('expanded');
+		that.classList.remove('collapsed');
+		that.classList.add('expanded');
 	}
+	return false;
+	
 }
 
 function form2json(form){
@@ -137,15 +156,7 @@ function submitComment(pf){
 }
 function docOnLoad(){
 	const expandoButtons = document.getElementsByClassName('expando-button');
-	for (let i = 0;i < expandoButtons.length;i++){
-		const b = expandoButtons[i];
-		const id = b.dataset.id;
-		b.addEventListener("click",e => {
-			e.preventDefault();
-			userTextDisplay(id);
-			
-		});
-	}
+	
 	
 	const optionButtons = document.getElementsByClassName('options_link');
 	
@@ -162,13 +173,7 @@ function docOnLoad(){
 	
 	const links = document.getElementsByClassName('link');
 	for (link of links){
-		const editButton = link.getElementsByClassName('editButton')[0];
-		if(editButton){
-			editButton.addEventListener("click",e => {
-				e.preventDefault();
-				editDisplay(editButton);
-			});
-		}
+		
 		const uf = link.getElementsByClassName('usertext')[0];
 		uf.addEventListener("submit",e => {
 			e.preventDefault();
